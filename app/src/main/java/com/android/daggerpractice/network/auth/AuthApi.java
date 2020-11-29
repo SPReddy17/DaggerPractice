867823b5fcc6b61bf3e5ccb9f639969c02f7ebd1
sp <saiprasadreddy081@gmail.com>
2020-11-28 20:46:55 -0500

    Retrofit calls and RxJava

--- app/src/main/java/com/android/daggerpractice/network/auth/AuthApi.java
+++ app/src/main/java/com/android/daggerpractice/network/auth/AuthApi.java
@@ -1,12 +1,18 @@
 package com.android.daggerpractice.network.auth;
 
+import com.android.daggerpractice.models.User;
+
+import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
 
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User> getUser(
+            @Path("id") int id
+    );
 }
--- app/src/main/java/com/android/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/android/daggerpractice/ui/auth/AuthViewModel.java
@@ -4,24 +4,49 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.android.daggerpractice.models.User;
 import com.android.daggerpractice.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
     private final AuthApi authapi;
 
     private static final String TAG = "AuthViewModel";
+
     @Inject
     public AuthViewModel(AuthApi authapi) {
         this.authapi = authapi;
         Log.d(TAG, "AuthViewModel: viewmodel is qworking...");
-        if(this.authapi == null){
-            Log.d(TAG, "AuthViewModel:  auth api is null");
-        }else{
-            Log.d(TAG, "AuthViewModel: auth api is not null");
-        }
+      authapi.getUser(1)
+              .toObservable()
+              .subscribeOn(Schedulers.io())
+              .subscribe(new Observer<User>() {
+                  @Override
+                  public void onSubscribe(Disposable d) {
+
+                  }
+
+                  @Override
+                  public void onNext(User user) {
+                      Log.d(TAG, "onNext: " + user.getEmail());
+                  }
+
+                  @Override
+                  public void onError(Throwable e) {
+                      Log.e(TAG, "onError: ", e  );
+                  }
+
+                  @Override
+                  public void onComplete() {
+
+                  }
+              });
     }
 
 

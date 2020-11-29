07e97206cd68d6355c7d991a8bd9e2eb214d318c
sp <saiprasadreddy081@gmail.com>
2020-11-28 23:02:12 -0500

    LiveData ReactiveStreams

--- app/src/main/java/com/android/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/android/daggerpractice/ui/auth/AuthViewModel.java
@@ -2,6 +2,10 @@ package com.android.daggerpractice.ui.auth;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.android.daggerpractice.models.User;
@@ -9,7 +13,6 @@ import com.android.daggerpractice.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
-import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
@@ -19,34 +22,30 @@ public class AuthViewModel extends ViewModel {
 
     private static final String TAG = "AuthViewModel";
 
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
     @Inject
     public AuthViewModel(AuthApi authapi) {
         this.authapi = authapi;
-        Log.d(TAG, "AuthViewModel: viewmodel is qworking...");
-      authapi.getUser(1)
-              .toObservable()
-              .subscribeOn(Schedulers.io())
-              .subscribe(new Observer<User>() {
-                  @Override
-                  public void onSubscribe(Disposable d) {
+        Log.d(TAG, "AuthViewModel: viewmodel is working...");
 
-                  }
+    }
 
-                  @Override
-                  public void onNext(User user) {
-                      Log.d(TAG, "onNext: " + user.getEmail());
-                  }
+    public void authenticateWithId(int userId){
 
-                  @Override
-                  public void onError(Throwable e) {
-                      Log.e(TAG, "onError: ", e  );
-                  }
-
-                  @Override
-                  public void onComplete() {
-
-                  }
-              });
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                authapi.getUser(userId)
+                .subscribeOn(Schedulers.io())
+        );
+        authUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                authUser.setValue(user);
+                authUser.removeSource(source);
+            }
+        });
+    }
+    public LiveData<User> observeUser(){
+        return authUser;
     }
 
 

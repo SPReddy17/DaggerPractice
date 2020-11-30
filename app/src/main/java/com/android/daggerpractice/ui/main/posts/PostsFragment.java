45b5850a562b1be5f8352a93cee0546a71941a5f
sp <saiprasadreddy081@gmail.com>
2020-11-30 11:25:28 -0500

    RecyclerView Dependency

--- app/src/main/java/com/android/daggerpractice/ui/main/posts/PostsFragment.java
+++ app/src/main/java/com/android/daggerpractice/ui/main/posts/PostsFragment.java
@@ -11,11 +11,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.daggerpractice.R;
 import com.android.daggerpractice.models.Post;
 import com.android.daggerpractice.ui.main.Resource;
+import com.android.daggerpractice.util.VerticalSpacingItemDecoration;
 import com.android.daggerpractice.viewmodels.ViewModelProviderFactory;
 
 import java.util.List;
@@ -26,8 +28,12 @@ import dagger.android.support.DaggerFragment;
 
 public class PostsFragment extends DaggerFragment {
     private static final String TAG = "PostsFragment";
+
     private PostsViewModel viewModel;
     private RecyclerView recyclerView;
+
+    @Inject
+    PostsRecyclerAdapter adapter;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -35,17 +41,20 @@ public class PostsFragment extends DaggerFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragments_posts,container,false);
-
+        return inflater.inflate(R.layout.fragments_posts, container, false);
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         recyclerView = view.findViewById(R.id.recycler_view);
-        viewModel = ViewModelProviders.of(this,providerFactory).get(PostsViewModel.class);
+
+        viewModel = ViewModelProviders.of(this, providerFactory).get(PostsViewModel.class);
+
+        initRecyclerView();
         subscribeObservers();
     }
-
 
     private void subscribeObservers(){
         viewModel.observePosts().removeObservers(getViewLifecycleOwner());
@@ -53,11 +62,32 @@ public class PostsFragment extends DaggerFragment {
             @Override
             public void onChanged(Resource<List<Post>> listResource) {
                 if(listResource != null){
-                    Log.d(TAG, "onChanged: " +listResource.data);
+                    switch (listResource.status){
+                        case LOADING:{
+                            Log.d(TAG, "onChanged: PostsFragment: LOADING...");
+                            break;
+                        }
+
+                        case SUCCESS:{
+                            Log.d(TAG, "onChanged: PostsFragment: got posts.");
+                            adapter.setPosts(listResource.data);
+                            break;
+                        }
+
+                        case ERROR:{
+                            Log.d(TAG, "onChanged: PostsFragment: ERROR... " + listResource.message);
+                            break;
+                        }
+                    }
                 }
             }
         });
     }
 
-
+    private void initRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        VerticalSpacingItemDecoration itemDecoration = new VerticalSpacingItemDecoration(15);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setAdapter(adapter);
+    }
 }
